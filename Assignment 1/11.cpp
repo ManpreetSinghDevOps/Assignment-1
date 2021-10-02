a04a9dd3dc82dd7502ae61084be4c8988845cd29
Sorav <Soravramgharia@gmail.com>
2021-10-02 18:57:59 +0530

    minner change in 11.cpp

--- Assignment 1/11.cpp
+++ Assignment 1/11.cpp
@@ -1,22 +1,23 @@
 // Write a program to check whether a entered character is lowercase ( a to z ) or uppercase ( A to Z )
 
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     char x;
-    cout<<"Enter a Character: ";
-    cin>>x;
-    cout<<x<<endl;
+    cout << "Enter a Character: ";
+    cin >> x;
+    cout << x << endl;
 
-    if ((x>65 && x<90) || (x>97&& x<122))
+    if ((x > 65 && x < 90) || (x > 97 && x < 122))
     {
-        cout<<"Yes "<<x<<" is a Character in ASCII Table"<<endl;
+        cout << "Yes " << x << " is a Character in ASCII Table" << endl;
     }
-    else{
-        cout<<"NO! "<<x<<" is Not a Character in ASCII Table"<<endl;
-        
+    else
+    {
+        cout << "NO! " << x << " is Not a Character in ASCII Table" << endl;
     }
-    
+
     return 0;
 }
